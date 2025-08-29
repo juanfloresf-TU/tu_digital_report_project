6cf5fb61350095cfdf42480846a7a7eee7005325
juanfloresf-TU <juan.floresf@televisaunivision.com>
2025-08-29 11:27:18 -0600

    Create date_trigger.py

---
+++ sprout/date_trigger.py
@@ -0,0 +1,14 @@
+from datetime import  date
+today = date.today()
+
+# Check if the day of the month is 1
+if today.day == 1:
+    print("It's the first day of the month!" + str(today))
+
+    ## append to dataset 
+    ## ejecutar mes anterior y guardar en dataset _dwn_m
+    
+    
+else:
+    print("It's not the first day of the month." + str(today))
+   
